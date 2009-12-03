d38b7c66562694e06f41bec59555ce599ed67162
Pirkka Hartikainen <pirkka@gmail.com>
2009-12-03 15:52:52 +0200

    drawing of planets happens... next up, colors and possibly titles

--- main.c
+++ main.c
@@ -20,7 +20,8 @@ int main() {
   int map_height = 930;
 
   int max_x, max_y, min_x, min_y;
-  int galaxy_width, galaxy_height, no_of_planets;
+  int galaxy_width, galaxy_height, no_of_planets, galaxy_measure;
+  int planet_x, planet_y, planet_height, planet_width, planet_radius;
 
   conn = mysql_init(NULL);
 
@@ -48,9 +49,20 @@ int main() {
 
   galaxy_width = max_x - min_x + 1;
   galaxy_height = max_y - min_y + 1;
-
+  if (galaxy_width > galaxy_height) {
+    galaxy_measure = galaxy_width;
+  } else {
+    galaxy_measure = galaxy_width;
+  }
   printf("GALAXY WIDTH: %i\n", galaxy_width);
   printf("GALAXY HEIGHT: %i\n", galaxy_height);
+  printf("GALAXY MEASUER: %i\n", galaxy_measure);
+
+
+  planet_width = map_width / galaxy_measure;
+  planet_height = map_height / galaxy_measure;
+  printf("PLANET WIDTH: %i\n", planet_width);
+  printf("PLANET HEIGHT: %i\n", planet_height);
 
   /*
     Initialize an image for us to draw on.
@@ -68,7 +80,7 @@ int main() {
   drawing_wand = NewDrawingWand();
 
    /* send SQL query for the actual planet info */
-  if (mysql_query(conn, "SELECT x,y,z,hue FROM planets LEFT JOIN players on planets.player_id = players.id LIMIT 10")) {
+  if (mysql_query(conn, "SELECT x,y,z,hue FROM planets LEFT JOIN players on planets.player_id = players.id")) {
     fprintf(stderr, "%s\n", mysql_error(conn));
     return(0);
   }
@@ -78,6 +90,10 @@ int main() {
   /* output fields 1 and 2 of each row */
   while ((row = mysql_fetch_row(res)) != NULL) {
     printf("%s %s %s (%s)\n", row[0], row[1], row[2], row[3]);
+    planet_x = atoi(row[0]) * planet_width + (map_width/2);
+    planet_y = atoi(row[1]) * planet_width + (map_height/2);
+    planet_radius = planet_width/2;
+    DrawCircle(drawing_wand, planet_x, planet_y, planet_x + planet_radius, planet_y + planet_radius);
   };
   /* Release memory used to store results and close connection */
   mysql_free_result(res);
